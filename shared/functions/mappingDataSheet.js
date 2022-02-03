33333e816e810a4335c46fd77a9d909ac278a307
Paul DAO <dao.van.paul@gmail.com>
2022-02-03 16:25:12 -0500

    FEAT mapping data receive from google and reorganize for the front app

---
+++ shared/functions/mappingDataSheet.js
@@ -0,0 +1,19 @@
+/**
+ * Mapping data receive from Google Spredsheet to render to front app
+ * @param values array of strings first array is label and rest is values
+ */
+export function mappingDataSheet(values) {
+    // We want to share a list of stackers with their skills
+    const keys = values[0];
+    // Remove first element of the array
+    values.shift();
+
+    return values.map(stacker => {
+        let data = {};
+        for (let i = 0; i < keys.length; i++) {
+            data[keys[i]] = stacker[i];
+        }
+        return data;
+    })
+
+}

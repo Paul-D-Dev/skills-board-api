2fd2553aa066f776ef771882333928beabd868a5
Paul DAO <dao.van.paul@gmail.com>
2022-02-03 15:13:43 -0500

    FEAT : Connect to SpreadSheet and get metadata

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { google } = require('googleapis');
+const app = express();
+const port = 3000;
+require('dotenv').config();
+
+app.get('/', async (req, res) => {
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+        scopes: 'https://www.googleapis.com/auth/spreadsheets'
+    })
+
+    // Create client instance for auth
+    const client = await auth.getClient();
+
+    // Instance of Google Sheet API
+    const sheets = google.sheets({version: 'v4', auth: client});
+    const spreadsheetId = process.env.SHEET_ID;
+
+    // Get metadata about spreadsheet
+    const metaData = await sheets.spreadsheets.get({
+        auth,
+        spreadsheetId
+    })
+    res.send(metaData.data);
+})
+
+app.listen(port, () => console.log('Express 3000'));
